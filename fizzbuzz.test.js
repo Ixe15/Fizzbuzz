23136bf07030906e02b625c703f45d211bfe1ea6
unknown <jariyawadeeniamnak@gmail.com>
2019-09-21 15:39:51 +0700

    add

---
+++ fizzbuzz.test.js
@@ -0,0 +1,8 @@
+// import { exportAllDeclaration } from "@babel/types"
+const Fizzbuzz = require("./fizzbuzz");
+describe("Test Fizzbuzz", () => {
+  test("Test 1 expect 1", () => {
+    const result = Fizzbuzz.say(1);
+    expect(result).toEqual(1); // Assert Function
+  });
+});
